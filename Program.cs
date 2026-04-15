200c41e37fbfe11558f059723664d5b5e5fc9c1d
Mariam Abdelrahman Youssef <156664021+Mariam0-0@users.noreply.github.com>
2026-04-15 17:08:02 +0200

    Q13

--- Program.cs
+++ Program.cs
@@ -192,6 +192,14 @@ namespace G_NET_9_LINQ02
             //int[] setB = { 3, 6, 9, 12, 15, 13 };
             //==============================================
 
+            //int[] setA = { 1, 3, 5, 7, 9, 11, 13 };
+            //int[] setB = { 3, 6, 9, 12, 15, 13 };
+
+            //var result = setA.Except(setB);
+            //foreach(int i in result)
+            //{
+            //    Console.WriteLine(i);
+            //}
             #endregion
 
             #region Question 14
